bfcac7e081c19445a176c7447fdeb5e877ec28a7
MK Kang <mkkang0116@gmail.com>
2018-09-04 21:18:02 -0700

    Scaffold

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNApplovinAd } = NativeModules;
+
+export default RNApplovinAd;
---
+++ ios/RNApplovinAd.m
@@ -0,0 +1,13 @@
+
+#import "RNApplovinAd.h"
+
+@implementation RNApplovinAd
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
